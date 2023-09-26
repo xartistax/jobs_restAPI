f85e838bbe7ef9571e9f217334e36b1919dba3c9
Demian Füglistaler <demianfueglistaler@icloud..com>
2023-09-26 12:17:14 +0200

    commented out middlewares

--- app.js
+++ app.js
@@ -2,39 +2,31 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser');
-
-
 /// Routes
 const jobRoutes = require('./routes/jobs');
 
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-// app.use(morgan('dev'))
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(bodyParser.json())
+app.use( (req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
 
-// app.use( (req,res,next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Methods', '*')
+        return res.status(200).json({})
+    }
 
-//     if (req.method === "OPTIONS") {
-//         res.header('Access-Control-Allow-Methods', '*')
-//         return res.status(200).json({})
-//     }
+    next()
 
-//     next()
-
-// })
-
+})
 app.use('/jobs', jobRoutes)
-
-
 app.use( (req,res,next) => {
     const error = new Error('Somethings wrong here')
     error.status = 404;
     next(error)
 } )
-
-
 app.use( (error , req,res,next) => {
     res.status(error.status || 500);
     res.json({
